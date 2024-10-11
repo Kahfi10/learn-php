a3e12ee7814b31a729231c9953f9600cd0bb7e65
Ashabul Kahfi <105841108523@student.unismuh.ac.id>
2024-10-11 14:44:48 +0800

    Refactor index.php to include variable declarations and server information

--- index.php
+++ index.php
@@ -6,8 +6,28 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
-    echo "Hello World!";
+    <!-- variables and data types -->
+     /*
+    <?php 
+    // Scalar types
+    $string = "Kahfi";
+    $int = 10;  
+    $float = 10.5;
+    $bool = true;
+    // Array
+    $array = array("Kahfi", 10, 10.5, true);
+
+    $nama = "Kahfi";
+
+    echo $_SERVER("DOCUMENT_ROOT");
+    echo "<br>";
+    echo $_SERVER("PHP_SELF");
+    echo "<br>";
+    echo $_SERVER("SERVER_NAME");
+    echo "<br>";
     ?>
+    <p>Halo aku <?php echo $nama ?>,Aku belajar</p> 
+
+
 </body>
 </html>
