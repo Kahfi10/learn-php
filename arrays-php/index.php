5c514da4f42ff5baaa79e4b28f61d76119770520
Ashabul Kahfi <105841108523@student.unismuh.ac.id>
2024-10-16 14:52:02 +0800

    Refactor index.php to modify array values and add associative arrays

--- arrays-php/index.php
+++ arrays-php/index.php
@@ -14,8 +14,34 @@
         "Orange",
         "Mango"
     ];
-
+    $fruits[1] = "Pineapple"; // change the value of the second element
+    echo "<br>";
+    unset($fruits[2]); // remove the third element
     echo $fruits[0];
+    echo $fruits[1];
+
+    echo "<br>";
+    $tasks = [
+        "Kahfi" => "Read Surah Al-Kahfi",
+        "Ashabul" => "Membuang sampah",
+        "Rumah" => "untuk di tinggali"
+    ];
+
+    echo $tasks["Kahfi"];
+    echo "<br>";
+    echo count($tasks);
+    echo "<br>";
+    print_r($tasks);
+    echo "<br>";
+    $tasks["kahfi"] = "tidur";
+
+    $food = [
+        "sayur" => array("bayam", "sawi", "kangkung"),
+        "buah" => array("apel", "mangga", "jeruk"),
+        "daging" => array("ayam", "sapi", "kambing")
+    ];
+
+    echo $food["sayur"][0];
     ?>
 </body>
 </html>
