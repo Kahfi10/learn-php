1c05b62d67c89e2ceefa35b088ab206e0cfd135d
Ashabul Kahfi <105841108523@student.unismuh.ac.id>
2024-10-18 19:56:32 +0800

    Refactor index.php to add user-defined function sayKahfi for greeting with name and age and to include a calculator function

--- user-defined-functions.php/index.php
+++ user-defined-functions.php/index.php
@@ -6,12 +6,23 @@
     <title>Document</title>
 </head>
 <body>
-    <?php 
+    <?php
+    // user defined function 
+    echo "<h1>User Defined Functions</h1>";
     function sayKahfi($name, $age) {
         return "Hello " . $name . ", you are " . $age . " years old";
     }
     
     echo sayKahfi("Kahfi", 25);
+    echo "<br>";
+    // function to calculate numbers
+    echo "<h1>Calculator</h1>";
+    function calculator(int $num01, int $num02) {
+        $result = $num01 + $num02;
+        return $result;
+    }
+
+    echo calculator(10, 20);
     ?>
 </body>
 </html>
