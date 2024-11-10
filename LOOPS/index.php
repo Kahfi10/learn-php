d77eb53f369c681a9c511724ff621a10bcc1485e
Ashabul Kahfi <105841108523@student.unismuh.ac.id>
2024-11-10 16:22:25 +0800

    Add while loop example to index.php alongside existing for loop

--- LOOPS/index.php
+++ LOOPS/index.php
@@ -7,9 +7,20 @@
 </head>
 <body>
     <?php 
-    
+    echo "For Loop ";
+    echo "<br>";
     for($i = 0; $i < 5; $i++) {
         echo "iterasi " . $i . "<br>";
+    }
+
+    echo "<br>";
+    echo "While Loop ";
+    echo "<br>";
+
+    $test = 4;
+    while($test < 12) {
+        echo "iterasi " . $test . "<br>";
+        $test++;
     }
     
     ?>
